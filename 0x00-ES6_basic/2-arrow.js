27f269afa94db56a6972ca06555108dfd9090079
Dennisco12 <akinwonjowodennisco@gmail.com>
2025-01-09 01:00:22 +0100

    Added task 0 to 8

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default class getNeighborhoodsList {
+    constructor() {
+        this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+    }
+    addNeighborhood(newNeighborhood) {
+      this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return this.sanFranciscoNeighborhoods;
+    };
+  }
